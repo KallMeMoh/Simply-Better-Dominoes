8b7986217a91965db358c2cbdb6751eeb842b73b
KallMeMoh <justmohammad66@gmail.com>
2025-07-07 19:04:31 +0300

    Remove unnecessary helper functions

--- src/helpers/generateToken.js
+++
@@ -1,5 +0,0 @@
-exports.generateAccessToken = (userId) => {
-  return jwt.sign({ userId }, config.jwt.accessSecret, {
-    expiresIn: config.jwt.accessTokenExpiry,
-  });
-};
--- src/helpers/measureLatency.js
+++
@@ -1,4 +0,0 @@
-module.exports = async () => {
-  const start = Date.now();
-  return Date.now() - start;
-};
