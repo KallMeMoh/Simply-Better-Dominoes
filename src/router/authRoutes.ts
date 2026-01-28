4987899dd517fe7421621b06a0c491ed3f5938e3
KallMeMoh <justmohammad66@gmail.com>
2026-01-28 23:29:43 +0200

    Stop validation on first error with bail()

--- src/router/authRoutes.ts
+++ src/router/authRoutes.ts
@@ -10,31 +10,39 @@ import {
 const authRouter = Router();
 
 authRouter.post(
-  '/signup',
+  '/register',
   [
     body('username')
       .notEmpty()
       .withMessage('Username is required')
+      .bail()
       .isLength({ min: 3, max: 20 })
       .withMessage('Username must be at least 3 characters long')
+      .bail()
       .matches(/^[a-zA-Z0-9_]+$/)
       .withMessage(
         'Username can only contain letters, numbers, and _ (underscores)',
-      ),
+      )
+      .bail(),
     body('email')
       .notEmpty()
       .withMessage('Email is required')
+      .bail()
       .isEmail()
-      .withMessage('Invalid email format'),
+      .withMessage('Invalid email format')
+      .bail(),
     body('password')
       .notEmpty()
       .withMessage('Password is required')
+      .bail()
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be at least 8 characters long')
+      .bail()
       .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?(_|[^\W\s])).+$/)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, and one number, and special characters',
-      ),
+      )
+      .bail(),
   ],
   signupController,
 );
@@ -46,27 +54,35 @@ authRouter.post(
       body('username')
         .notEmpty()
         .withMessage('Username is required')
+        .bail()
         .isLength({ min: 3, max: 20 })
         .withMessage('Username must be at least 3 characters long')
+        .bail()
         .matches(/^[a-zA-Z0-9_]+$/)
         .withMessage(
           'Username can only contain letters, numbers, and _ (underscores)',
-        ),
+        )
+        .bail(),
       body('email')
         .notEmpty()
         .withMessage('Email is required')
+        .bail()
         .isEmail()
-        .withMessage('Invalid email format'),
+        .withMessage('Invalid email format')
+        .bail(),
     ]),
     body('password')
       .notEmpty()
       .withMessage('Password is required')
+      .bail()
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be at least 8 characters long')
+      .bail()
       .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?(_|[^\W\s])).+$/)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, and one number, and special characters',
-      ),
+      )
+      .bail(),
   ],
   loginController,
 );
@@ -88,30 +104,39 @@ authRouter.post(
     body('curr_password')
       .notEmpty()
       .withMessage('Old Password is required')
+      .bail()
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be at least 8 characters long')
+      .bail()
       .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?(_|[^\W\s])).+$/)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, and one number, and special characters',
-      ),
+      )
+      .bail(),
     body('new_password')
       .notEmpty()
       .withMessage('New Password is required')
+      .bail()
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be at least 8 characters long')
+      .bail()
       .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?(_|[^\W\s])).+$/)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, and one number, and special characters',
-      ),
+      )
+      .bail(),
     body('re_password')
       .notEmpty()
       .withMessage('Repeat New Password is required')
+      .bail()
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be at least 8 characters long')
+      .bail()
       .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?(_|[^\W\s])).+$/)
       .withMessage(
         'Password must contain at least one uppercase letter, one lowercase letter, and one number, and special characters',
-      ),
+      )
+      .bail(),
   ],
   changePasswordController,
 );
