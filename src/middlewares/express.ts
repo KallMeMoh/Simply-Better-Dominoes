e456928f7ecc2459a00aecf5467a2fb08538ac7d
KallMeMoh <justmohammad66@gmail.com>
2026-02-05 16:38:03 +0200

    Fix protectedEnpoint middleware

--- src/middlewares/express.ts
+++ src/middlewares/express.ts
@@ -13,19 +13,23 @@ export async function protectedEndpoint(
   try {
     const token: string = req.cookies['token'];
     console.log(token);
-    if (!token) throw new Error('unauthorized');
+    if (!token) return res.status(401).json({ msg: 'Unauthorized' });
 
-    const { userId, sessionId, tokenVersion } = jwt.verify(
-      token,
-      jwtConfig.secret,
-    ) as JwtPayload;
+    const payload = jwt.verify(token, jwtConfig.secret) as JwtPayload;
+    if (
+      !payload ||
+      !payload.userId ||
+      !payload.sessionId ||
+      !payload.tokenVersion
+    )
+      return res.status(401).json({ msg: 'Malformed cookie' });
 
     const [user, session] = await Promise.all([
-      User.findById(userId),
-      Session.findById(sessionId),
+      User.findById(payload.userId),
+      Session.findById(payload.sessionId),
     ]);
-    if (!user || user.token_version !== tokenVersion || !session)
-      throw new Error('malformed cookie');
+    if (!user || user.token_version !== payload.tokenVersion || !session)
+      return res.status(401).json({ msg: 'Malformed cookie' });
 
     // TODO: flag session as sus if client info are different from session
     // if (
@@ -38,7 +42,11 @@ export async function protectedEndpoint(
     req.session = session;
     next();
   } catch (err: any) {
-    if (err.name === 'JsonWebTokenError') return res.sendStatus(500);
+    if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+      return res.status(401).json({ msg: 'Invalid or expired token' });
+    }
+
+    next(err);
   }
 }
 
